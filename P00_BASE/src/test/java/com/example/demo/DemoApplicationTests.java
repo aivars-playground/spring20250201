7e695c1ff98f0b8c9e7bfe95c1dd20f8b3d102a6
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-02-01 18:38:50 +0000

    initial commit

--- P00_BASE/src/test/java/com/example/demo/DemoApplicationTests.java
+++ P00_BASE/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.p01_basics;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class P01BasicsApplicationTests {
+class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
